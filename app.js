40f5788323a023a524bfe69cf627eb40362dd6cf
ZhengTzer <zhengtzer@gmail.com>
2020-12-29 12:59:30 +0800

    feat: add check cookie function and remove cookie when click logout

--- app.js
+++ app.js
@@ -5,27 +5,44 @@ const bodyParser = require('body-parser')
 const checkLogin = require('./function/checkLogin')
 const port = 3000
 const app = express()
+const cookieParser = require('cookie-parser')
 
 // engine setting
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
+app.use(cookieParser('ac'))
 
 // login page
 app.get('/', (req, res) => {
-  res.render('index')
+  result = req.signedCookies.firstName
+  // check cookie
+  if (req.signedCookies.firstName) {
+    res.render('welcome', { result })
+  } else {
+    res.render('index')
+  }
 })
 
 app.post('/', (req, res) => {
+  // declare
   const userInput = req.body
   const result = checkLogin(req.body)
 
+  // check login
   if (result.includes('incorrect')) {
     res.render('index', { result, userInput })
   } else {
+    res.cookie('firstName', result, { signed: true })
     res.render('welcome', { result })
   }
+})
+
+// logout
+app.post('/logout', (req, res) => {
+  res.clearCookie('firstName')
+  res.redirect('/')
 })
 
 // port listening
